c3230f037927f416f9514ef40dd4dcb686550c10
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2022-10-19 17:38:24 -0300

    Fetch API

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,48 +1,12 @@
-const criaNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement("tr");
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-            <td>${email}</td>
-            <td>
-                <ul class="tabela__botoes-controle">
-                    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                </ul>
-            </td>
-            `;
-    linhaNovoCliente.innerHTML = conteudo;
-    return linhaNovoCliente;
-};
-const tabela = document.querySelector("[data-tabela]");
-
+/**inicializando a fetch */
 const listaClientes = () => {
-    /**inicializando a promise */
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest();
-
-        http.open("GET", "http://localhost:3000/profile");
-
-        http.onload = () => {
-            /**funcao anonima dentro da função onload chamasse Callback dentro de função assíncrona*/
-            if (http.status >= 400) {
-                /**acima de 400 bad request */
-
-                reject(JSON.parse(http.response));
-            } else {
-                resolve(JSON.parse(http.response));
-            }
-        };
-        http.send();
-    });
-        
-    return promise;
-};
-
-listaClientes()
-.then(data =>{
-   
-data.forEach((elemento) => {
-    tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
-})})
-
+    /**por padrão faz get e devolve promise */
+    return fetch(`http://localhost:3000/profile`)
+    .then((resposta) => {
+        return resposta.json();
+    })
+}
+export const clienteService = {
+    listaClientes
+}
 
