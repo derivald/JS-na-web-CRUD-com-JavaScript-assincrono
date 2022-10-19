83d9c95260662ca53314eb4fea28e1a710042171
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2022-10-19 19:43:00 -0300

    Criar e remover clientes

--- projeto_inicial/admin/service/cliente-service.js
+++ projeto_inicial/admin/service/cliente-service.js
@@ -1,12 +1,35 @@
 /**inicializando a fetch */
 const listaClientes = () => {
     /**por padrão faz get e devolve promise */
-    return fetch(`http://localhost:3000/profile`)
-    .then((resposta) => {
+    return fetch(`http://localhost:3000/profile`).then((resposta) => {
         return resposta.json();
+    });
+};
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email,
+        })
     })
+    .then(resposta => {
+        return resposta.body
+    });
+};
+const removeCliente = (id)=> {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+
+    method: 'DELETE'
+})
+
 }
 export const clienteService = {
-    listaClientes
-}
-
+    listaClientes,
+    criaCliente,
+    removeCliente
+    
+};
