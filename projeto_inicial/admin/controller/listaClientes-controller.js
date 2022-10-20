52fd9e755f4433b10313488822094c575a6a3189
Derivaldo Brito <derivaldo.brito@yahoo.com.br>
2022-10-20 16:45:03 -0300

    Editar dados

--- projeto_inicial/admin/controller/listaClientes-controller.js
+++ projeto_inicial/admin/controller/listaClientes-controller.js
@@ -6,7 +6,7 @@ const criaNovaLinha = (nome, email, id) => {
             <td>${email}</td>
             <td>
                 <ul class="tabela__botoes-controle">
-                    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                    <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
                     <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
                 </ul>
             </td>
@@ -20,9 +20,9 @@ const tabela = document.querySelector("[data-tabela]");
 
 tabela.addEventListener("click", (evento) => {
     let ehBotaoDeletar =
-        evento.target.className === "botao-simples botao-simples--excluir";
+        evento.target.className === "botao-simples botao-simples--excluir"
         if (ehBotaoDeletar) {
-            const linhaCliente = evento.target.closest("[data-id]");
+            const linhaCliente = evento.target.closest("[data-id]")
             let id = linhaCliente.dataset.id
             clienteService.removeCliente(id)
             .then(() => {
